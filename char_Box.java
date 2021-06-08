26119002e3ac9f0ac2aaf03b4985e98e181faab3
vikkyvivek2002 <84318895+vikkyvivek2002@users.noreply.github.com>
2021-06-08 14:25:26 +0530

    Add files via upload

---
+++ char_Box.java
@@ -0,0 +1,15 @@
+class char_Box
+{
+public static void main(String[] args)
+{
+ for(int i=1; i<=5; i++)
+{
+  for(char j='E'; j>='A'; j--)
+{
+System.out.print(j+" ");
+
+}
+System.out.println();
+}
+}
+}
